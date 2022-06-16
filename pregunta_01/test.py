9c31dfb041e73c77b332e639b89cefd10ffc5335
Jalena Correa <95106060+jcorreaan@users.noreply.github.com>
2022-06-15 23:33:58 -0500

    Add files via upload

--- pregunta_01/test.py
+++ pregunta_01/test.py
@@ -4,34 +4,21 @@ import os
 from os.path import dirname
 
 module_path = dirname(__file__)
-# os.chdir(module_path)
-
-os.path.dirname(os.path.abspath(__file__))
+os.chdir(module_path)
 
 expected = [
-    "B,2015-09-23,1",
-    "B,2017-03-09,2",
-    "B,2017-11-19,9",
-    "B,2017-03-10,10",
-    "B,2018-06-15,12",
-    "B,2017-06-05,15",
-    "C,2017-02-18,5",
-    "C,2015-02-01,8",
-    "D,2017-11-11,3",
-    "D,2016-07-03,4",
-    "D,2017-11-03,7",
-    "D,2017-09-27,13",
-    "D,2017-08-25,14",
-    "E,2015-11-28,6",
-    "E,2015-08-02,11",
+    "B,6",
+    "C,2",
+    "D,5",
+    "E,2",
 ]
 
-# if os.path.isdir("output"):
-#    os.system("rm -rf output")
-#
-# os.system("docker run -v $PWD:/workspace jdvelasq/pig:classroom")
+if os.path.isdir("output"):
+    os.system("rm -rf output")
 
-# assert os.path.isdir("output") is True
+os.system("docker run -v $PWD:/workspace jdvelasq/pig:classroom")
+
+assert os.path.isdir("output") is True
 
 result = []
 with fileinput.input(files=glob.glob("output/*")) as f:
